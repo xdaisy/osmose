0b448586f4951a60b1220afa128581feed6b39ff
Daisy <daisy1xu4@gmail.com>
2018-09-25 22:35:23 -0700

    set up a handler to keep track of all the events that occurred and display dialogue depending on if there is a specific event we want separate dialogue for

--- Osmose/Assets/Scripts/Interaction/InteractionObject.cs
+++ Osmose/Assets/Scripts/Interaction/InteractionObject.cs
@@ -6,15 +6,27 @@ public class InteractionObject : MonoBehaviour {
 
     public bool talks; // if true, object here can talk to player
 
+    public string specificEvent; // name of specific event for specific dialogue
+
     private Dialogue dMang; // dialogue manager that will send the message to
 
-    public string[] dialogueLines; // lines of dialogue
+    public string[] preEventDialogue; // lines of generic dialogue
 
-    public string personTalkingName; // name of person talking in dialogue
+    public string[] postEventDialogue; // lines of dialogue for before/after specific event
+
+    private string personTalking; // name of person talking in dialogue
 
     // Use this for initialization
     void Start() {
         dMang = FindObjectOfType<Dialogue>();
+        if (this.CompareTag("NPC")) {
+            // if this is an npc, npc is talking
+            personTalking = this.name;
+        } else {
+            // else this the player's thoughts/observation
+            GameObject player = GameObject.FindWithTag("Player");
+            personTalking = player.name;
+        }
     }
 
     // Update is called once per frame
@@ -24,17 +36,23 @@ public class InteractionObject : MonoBehaviour {
 
     // talk if this object has a message
     public void Talk() {
-        //dialogue.showText(message);
-        //Debug.Log(message);
         if (!dMang.getDialogueActive()) {
             // shows if dialogue already not showing
-            //dMang.dialogueLines = this.dialogueLines;
-            dMang.setDialogueLines(this.dialogueLines);
+
+            if (!this.specificEvent.Equals("") && CutSceneHandler.didEventHappened(specificEvent)) {
+                // there is a specified event AND event happened
+                dMang.setDialogueLines(this.postEventDialogue);
+            } else {
+                // event did not happened or there is no specified event
+                dMang.setDialogueLines(this.preEventDialogue);
+            }
+
             dMang.setCurrentLine(0);
-            dMang.setName(personTalkingName);
+            dMang.setName(personTalking);
             dMang.ShowDialogue();
         } else {
             // if dialogue is showing, go to next line
+
             dMang.ShowNextLine();
         }
     }
