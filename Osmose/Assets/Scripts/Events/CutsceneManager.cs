2cf63a1366892680911e62635d0eb6f59c73a575
d_xu <d_xu@wargaming.net>
2018-10-03 12:38:24 -0700

    cutscene read from txt file and display text to it along with changing the person's name

---
+++ Osmose/Assets/Scripts/Events/CutsceneManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class CutsceneManager : MonoBehaviour {
+
+    public string cutsceneTxtPath;
+
+    private FileInfo sourceFile;
+
+    private StreamReader reader;
+
+    public Text dText;
+    public Text dName;
+
+	// Use this for initialization
+	void Start () {
+        sourceFile = new FileInfo(cutsceneTxtPath); // get file
+        reader = sourceFile.OpenText(); // open the file to read
+
+        // get name of person saying first line of dialogue in cutscene
+        string line = reader.ReadLine();
+        string[] person = line.Split('-');
+        dName.text = person[0];
+
+        // get first line of dialogue in cutscene
+        line = reader.ReadLine();
+        dText.text = line;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Interact")) {
+
+            if(reader.EndOfStream) {
+                // if reader is at the end of the file, don't do anything
+                return;
+            }
+
+            string line = reader.ReadLine();
+            while(line.Length == 0) {
+                // don't show empty strings
+                line = reader.ReadLine();
+            }
+            if (line.Contains(":")) {
+                // if line contains :, then is name
+                string[] person = line.Split('-');
+
+                dName.text = person[0]; // name of person talking is always first word
+                line = reader.ReadLine(); // name of person talking always followed by lines of text
+            }
+
+            dText.text = line;
+        }
+	}
+}
