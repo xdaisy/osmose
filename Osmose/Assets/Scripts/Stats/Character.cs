1742b76e1b6218423376254feefe756c2a28de66
d_xu <d_xu@wargaming.net>
2018-11-20 13:17:41 -0800

    working on party stats class (is static?)

---
+++ Osmose/Assets/Scripts/Stats/Character.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character {
+
+    private int level;
+    private int currExp;
+    private int nextExp;
+
+    private Stat currentHP;
+    private Stat currentSP;
+    private Stat maxHP;
+    private Stat maxSP;
+    private Stat attack;
+    private Stat defense;
+    private Stat speed;
+    private Stat luck;
+
+    private Object weapon;
+    private Object armor;
+
+    public Character(float hp, float sp, float attack, float defense, float speed, float luck) {
+        this.level = 1;
+        this.currExp = 0;
+        this.nextExp = 10;
+
+        this.currentHP = new Stat(hp);
+        this.maxHP = new Stat(hp);
+        this.currentSP = new Stat(sp);
+        this.maxSP = new Stat(sp);
+        this.attack = new Stat(attack);
+        this.defense = new Stat(defense);
+        this.speed = new Stat(speed);
+        this.luck = new Stat(luck);
+    }
+
+    /// <summary>
+    /// Get character's level
+    /// </summary>
+    /// <returns></returns>
+    public int GetLevel() {
+        return this.level;
+    }
+
+    /// <summary>
+    /// Get character's current amount of HP
+    /// </summary>
+    /// <returns></returns>
+    public float GetCurrentHP() {
+        return this.currentHP.Value;
+    }
+
+    /// <summary>
+    /// Get character's max HP
+    /// </summary>
+    /// <returns></returns>
+    public float GetMaxHP() {
+        return this.maxHP.Value;
+    }
+
+    /// <summary>
+    /// Get character's current amount of SP
+    /// </summary>
+    /// <returns></returns>
+    public float GetCurrentSP() {
+        return this.currentSP.Value;
+    }
+
+    /// <summary>
+    /// Get character's max SP
+    /// </summary>
+    /// <returns></returns>
+    public float GetMaxSP() {
+        return this.maxSP.Value;
+    }
+
+    /// <summary>
+    /// Get character's attack
+    /// </summary>
+    /// <returns></returns>
+    public float GetAttack() {
+        return this.attack.Value;
+    }
+
+    /// <summary>
+    /// Get character's defense
+    /// </summary>
+    /// <returns></returns>
+    public float GetDefense() {
+        return this.defense.Value;
+    }
+
+    /// <summary>
+    /// Get character's speed
+    /// </summary>
+    /// <returns></returns>
+    public float GetSpeed() {
+        return this.speed.Value;
+    }
+
+    /// <summary>
+    /// Get character's luck
+    /// </summary>
+    /// <returns></returns>
+    public float GetLuck() {
+        return this.luck.Value;
+    }
+
+    /// <summary>
+    /// Get the amount of EXP left before level up
+    /// </summary>
+    /// <returns></returns>
+    public int GetExpLeft() {
+        return this.nextExp - this.currExp;
+    }
+
+    /// <summary>
+    /// Increase the experience point gained
+    /// </summary>
+    /// <param name="exp">Amount of experience points gained</param>
+    public void GainExp(int exp) {
+        this.currExp += exp;
+
+        if (this.currExp >= this.nextExp) {
+            // if current amount of exp surpass amount of exp needed to next level, level up
+            levelUp();
+        }
+    }
+
+    /// <summary>
+    /// Recover character's HP
+    /// </summary>
+    /// <param name="hitpoint">Amount of HP to recover</param>
+    public void RecoverHP(float hitpoints) {
+        if (GetCurrentHP() + hitpoints > GetMaxHP()) {
+            currentHP.BaseValue = GetMaxHP();
+        } else {
+            currentHP.BaseValue += hitpoints;
+        }
+    }
+
+    private void levelUp() {
+
+    }
+}
---
+++ Osmose/Assets/Scripts/Stats/PartyStats.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartyStats {
+    private Dictionary<string, Character> party = new Dictionary<string, Character>();
+
+    public float GetCharacterCurrentHP(string name) {
+        return party[name].GetCurrentHP();
+    }
+
+    public float GetCharacterMaxHP(string name) {
+        return party[name].GetMaxHP();
+    }
+
+    public float GetCharacterCurrentSp(string name) {
+        return party[name].GetCurrentSP();
+    }
+
+    public float GetCharacterMaxSp(string name) {
+        return party[name].GetMaxSP();
+    }
+
+    public float GetCharacterAttack(string name) {
+        return party[name].GetAttack();
+    }
+
+    public float GetCharacterDefense(string name) {
+        return party[name].GetDefense();
+    }
+
+    public float GetCharacterSpeed(string name) {
+        return party[name].GetSpeed();
+    }
+
+    public float GetCharacterLuck(string name) {
+        return party[name].GetLuck();
+    }
+
+    public void GainExperience(string[] party, int expPoints) {
+        foreach(string name in party) {
+            this.party[name].GainExp(expPoints);
+        }
+    }
+
+    public void RecoverHP(string[] characters, float hitpoints) {
+        foreach(string name in characters) {
+            party[name].RecoverHP(hitpoints);
+        }
+    }
+}
