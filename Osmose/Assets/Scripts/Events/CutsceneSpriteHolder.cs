8d0b35dfdffb0b88d713f97f532348e6d08f0b65
Daisy <daisy1xu4@gmail.com>
2018-11-29 23:43:52 -0800

    needed to move the initialization of the dictionary to awake, not start

--- Osmose/Assets/Scripts/Events/CutsceneSpriteHolder.cs
+++ Osmose/Assets/Scripts/Events/CutsceneSpriteHolder.cs
@@ -9,6 +9,10 @@ public class CutsceneSpriteHolder : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        
+    }
+
+    private void Awake() {
         sprites = new Dictionary<string, Sprite>();
         foreach (Sprite sprite in Sprites) {
             sprites[sprite.name] = sprite;
@@ -16,15 +20,6 @@ public class CutsceneSpriteHolder : MonoBehaviour {
     }
 
     public Sprite GetSprite(string name) {
-        //return sprites[name];
-        Sprite sprite = Sprites[0];
-        for (int i = 1; i < Sprites.Count; i++) {
-            Sprite s = Sprites[i];
-            if (s.name == name) {
-                sprite = s;
-                break;
-            }
-        }
-        return sprite;
+        return sprites[name];
     }
 }
