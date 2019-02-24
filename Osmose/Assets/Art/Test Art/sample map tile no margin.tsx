adbbbc5cfefdf53b865a0a0daa5ce93f780b96b9
daisyx <daisy1xu4@gmail.com>
2019-02-23 20:50:12 -0800

    put in test meeting hall pic drew test geoffrey

--- Osmose/Assets/Art/Test Art/sample map tile no margin.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="sample map tile no margin" tilewidth="128" tileheight="128" spacing="1" tilecount="4" columns="2">
- <image source="sample map tile no margin.png" width="258" height="258"/>
-</tileset>
