d9d62be406b7788847a3cd234a83e056bf39ac73
daisyx <daisy1xu4@gmail.com>
2019-04-06 09:54:15 -0700

    can use left and right to -/+ 10 to amount want to buy update amount of money owned when buying and selling

--- Osmose/Assets/Scripts/Shop/AmountPanel.cs
+++ Osmose/Assets/Scripts/Shop/AmountPanel.cs
@@ -8,10 +8,15 @@ using System;
 public class AmountPanel : MonoBehaviour {
     public Text Amount;
     public Text TotalAmount;
+    public Text MoneyOwned;
 
     private int amount = 0;
     private Items currItem = null;
     private bool isBuying = true;
+
+    private void Awake() {
+        MoneyOwned.text = "" + GameManager.Instance.Wallet;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -19,8 +24,7 @@ public class AmountPanel : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         if (this.gameObject.activeSelf) {
             // if this is visible
             if (Input.GetButtonDown("Vertical")) {
@@ -34,16 +38,60 @@ public class AmountPanel : MonoBehaviour {
                 if (buttonInput > 0.5f) {
                     // increase amount
                     int moneyOwned = GameManager.Instance.Wallet;
-                    if (isBuying && moneyOwned >= (amount + 1) * currItem.Value) {
+                    int owned = 0;
+                    if (currItem.IsItem) {
+                        owned = GameManager.Instance.GetAmountOfItem(currItem.ItemName);
+                    } else {
+                        owned = GameManager.Instance.GetAmountOfEquipment(currItem.ItemName);
+                    }
+                    if (isBuying && moneyOwned >= (amount + 1) * currItem.Value && owned + amount + 1 <= 99) {
                         amount++;
                         updateAmount();
                     }
-                    if (!isBuying && currItem.IsItem && GameManager.Instance.GetAmountOfItem(currItem.ItemName) > amount) {
+                    if (!isBuying && currItem.IsItem && owned > amount) {
                         amount++;
                         updateAmount();
                     }
-                    if (!isBuying && (currItem.IsWeapon || currItem.IsArmor) && GameManager.Instance.GetAmountOfEquipment(currItem.ItemName) > amount) {
-                        amount++;
+                }
+            }
+            if (Input.GetButtonDown("Horizontal")) {
+                float buttonInput = Input.GetAxisRaw("Horizontal");
+
+                if (buttonInput < -0.5f && amount > 0) {
+                    // - 10
+                    amount -= 10;
+                    amount = Math.Max(amount, 0); // min = 0
+                    updateAmount();
+                }
+                if (buttonInput > 0.5f) {
+                    // + 10
+                    int moneyOwned = GameManager.Instance.Wallet;
+                    int owned = 0;
+                    int cost = 0;
+
+                    if (currItem.IsItem) {
+                        owned = GameManager.Instance.GetAmountOfItem(currItem.ItemName);
+                    } else {
+                        owned = GameManager.Instance.GetAmountOfEquipment(currItem.ItemName);
+                    }
+
+                    int add = 0;
+
+                    if (isBuying) {
+                        // calculate if add 10 or add some other amount
+                        add = 99 - (owned + amount) >= 10 ? 10 : 99 - (owned + amount);
+                        add = Math.Max(add, 0); // make sure doesn't go below 0
+                        cost = (amount + add) * currItem.Value;
+                        while (cost > moneyOwned && add > 0) {
+                            add--;
+                            cost = (amount + add) * currItem.Value;
+                        }
+                    } else {
+                        add = owned - amount >= 10 ? 10 : owned - amount;
+                    }
+
+                    if (add > 0) {
+                        amount += add;
                         updateAmount();
                     }
                 }
@@ -51,6 +99,11 @@ public class AmountPanel : MonoBehaviour {
         }
     }
 
+    /// <summary>
+    /// Set up which Item and whether or not it is being bought
+    /// </summary>
+    /// <param name="item">Item that is being bought or sold</param>
+    /// <param name="isBuying">Flag for whether or not player is buying</param>
     public void OpenAmountPanel(Items item, bool isBuying) {
         currItem = item;
         this.isBuying = isBuying;
@@ -58,6 +111,9 @@ public class AmountPanel : MonoBehaviour {
         EventSystem.current.SetSelectedGameObject(Amount.gameObject);
     }
 
+    /// <summary>
+    /// Set variables to default when exiting Amount Panel
+    /// </summary>
     public void CloseAmountPanel() {
         amount = 0;
         TotalAmount.text = "0";
@@ -65,6 +121,9 @@ public class AmountPanel : MonoBehaviour {
         isBuying = true;
     }
 
+    /// <summary>
+    /// Buy or Sell the amount of selected item
+    /// </summary>
     public void BuySell() {
         int totalAmount = Int32.Parse(TotalAmount.text);
 
@@ -91,5 +150,6 @@ public class AmountPanel : MonoBehaviour {
         } else {
             TotalAmount.text = "" + (amount * Mathf.Max(Mathf.RoundToInt(currItem.Value * 0.5f), 1));
         }
+        MoneyOwned.text = "" + GameManager.Instance.Wallet;
     }
 }
