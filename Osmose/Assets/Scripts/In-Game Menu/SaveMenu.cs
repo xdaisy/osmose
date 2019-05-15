908df379666563859b0d8a30a0b1c03633c9325d
daisyx <daisy1xu4@gmail.com>
2019-05-14 22:59:57 -0700

    made save menu for saving in game

---
+++ Osmose/Assets/Scripts/In-Game Menu/SaveMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveMenu : MonoBehaviour {
+    [Header("General")]
+    public Button[] SaveFiles;
+    public Text[] NoData;
+    public GameObject[] Data;
+
+    [Header("Save Data UI")]
+    public Text[] PlayTime;
+    public Text[] Location;
+    public Image[] PartyMember1;
+    public Image[] PartyMember2;
+    public Image[] PartyMember3;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
