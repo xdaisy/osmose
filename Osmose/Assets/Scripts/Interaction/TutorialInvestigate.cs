717934cb72ef28c031383c1e302c01efa85d186c
daisyx <daisy1xu4@gmail.com>
2020-07-25 17:08:58 -0700

    set up tutorial flow

---
+++ Osmose/Assets/Scripts/Interaction/TutorialInvestigate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialInvestigate : MonoBehaviour {
+    [SerializeField] private SceneName sceneToLoad;
+    [SerializeField] private string[] dialogue;
+
+    private bool canActivate = false;
+    private bool showingDialogue = false;
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetButtonDown("Interact") && canActivate && !showingDialogue) {
+            showingDialogue = true;
+            Dialogue.Instance.ShowDialogue(dialogue, false);
+        } else if (showingDialogue && !Dialogue.Instance.GetDialogueActive()) {
+            LoadSceneLogic.Instance.LoadScene(sceneToLoad.GetSceneName());
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player") {
+            canActivate = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.tag == "Player") {
+            canActivate = false;
+        }
+    }
+}
--- Osmose/Assets/Scripts/SceneChanges/CutsceneEntrance.cs
+++ Osmose/Assets/Scripts/SceneChanges/CutsceneEntrance.cs
@@ -1,14 +1,14 @@
 ﻿using UnityEngine;
 
 public class CutsceneEntrance : MonoBehaviour {
-    public string transitionFromCutscene;
+    public SceneName transitionFromCutscene;
 
     public bool ShowDialogue;
     public string[] PostDialogue;
 
     // Use this for initialization
     void Start() {
-        if (transitionFromCutscene.Equals(GameManager.Instance.PreviousScene)) {
+        if (transitionFromCutscene.GetSceneName().Equals(GameManager.Instance.PreviousScene)) {
             // set player to entrance's position
             PlayerControls.Instance.SetPosition(transform.position);
 
