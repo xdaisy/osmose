87ec9282b5f2d47e6f9fa9d1d062b04860e0defc
daisyx <daisy1xu4@gmail.com>
2020-08-09 21:05:38 -0700

    npc faces player when player is speaking with them

---
+++ Osmose/Assets/Scripts/Interaction/NPC.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Class for the NPC behaviors
+/// </summary>
+public class NPC : MonoBehaviour {
+    public SpriteRenderer NpcSprite;
+    [SerializeField] private Sprite DownSprite;
+    [SerializeField] private Sprite UpSprite;
+    [SerializeField] private Sprite LeftSprite;
+    [SerializeField] private Sprite RightSprite;
+
+    private bool canInteract = false;
+    private Vector2 direction;
+
+    // Update is called once per frame
+    void Update() {
+        if (canInteract && Input.GetButtonDown("Interact")) {
+            // if interacting with npc
+            if (direction.y > 0) {
+                // npc face up
+                NpcSprite.sprite = UpSprite;
+            } else if (direction.y < 0) {
+                // npc face down
+                NpcSprite.sprite = DownSprite;
+            } else if (direction.x > 0) {
+                // npc face right
+                NpcSprite.sprite = RightSprite;
+            } else if (direction.x < 0) {
+                // npc face left
+                NpcSprite.sprite = LeftSprite;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player") {
+            direction = other.GetComponent<PlayerControls>().GetLastMove() * -1; // want the direction to be the inverse of the player's last movement
+            canInteract = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.tag == "Player") {
+            canInteract = false;
+        }
+    }
+}
