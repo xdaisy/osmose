3b8e332bb1595c90d29498d59c6a5750d428014b
daisyx <daisy1xu4@gmail.com>
2021-06-26 11:28:12 -0700

    set up dialogue to go through flow for going to the logic puzzle

---
+++ Osmose/Assets/Scripts/Interaction/ActivatePuzzle.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class ActivatePuzzle : MonoBehaviour {
+    public string[] InitialDialogue;
+    public string[] YesDialogue;
+    public string[] NoDialogue;
+    public string[] CannotDialogue;
+    public int NumClues;
+    public SceneName sceneToLoad;
+
+    private bool canActivate = false;
+
+    // Update is called once per frame
+    void Update() {
+        if (canActivate && GameManager.Instance.CanStartDialogue() && Input.GetButtonDown("Interact") && !Dialogue.Instance.dBox.activeSelf) {
+            bool canDoPuzzle = GameManager.Instance.GetCurrentClues().Count == NumClues;
+            Dialogue.Instance.ActivatePuzzleDialogue(InitialDialogue, YesDialogue, NoDialogue, CannotDialogue, canDoPuzzle, sceneToLoad.GetSceneName());
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player") {
+            canActivate = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        if (other.tag == "Player") {
+            canActivate = false;
+        }
+    }
+}
--- Osmose/Assets/Scripts/Interaction/Dialogue.cs
+++ Osmose/Assets/Scripts/Interaction/Dialogue.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class Dialogue : MonoBehaviour {
     public static Dialogue Instance; // dialogue manager instance
@@ -13,30 +14,62 @@ public class Dialogue : MonoBehaviour {
     public GameObject BlackBG;
     public Image ClueImage;
 
+    [Header("Confirmation popup")]
+    public GameObject ConfirmationPopup;
+    public GameObject YesButton;
+
     private string[] dialogueLines; // lines of dialogue
     private int currentLine; // current line of dialogue
 
     private bool justStarted; // keep track if the dialogue just got started
+
+    private EventSystem eventSystem;
+
+    private bool needInput;
+    private string[] yesDialogue;
+    private string[] noDialogue;
+    private string[] cannotDialogue;
+    private bool canDoPuzzle;
+    private string sceneName;
+    private bool doChangeScene;
 
     // Start is called before the first frame update
     void Start() {
         if (Instance == null) {
             Instance = this;
         }
+        eventSystem = this.GetComponent<EventSystem>();
     }
 
     // Update is called once per frame
     void Update () {
-        if (dBox.activeSelf && Input.GetButtonUp("Interact")) {
+        if (!ConfirmationPopup.activeSelf && dBox.activeSelf && Input.GetButtonUp("Interact")) {
             if (!justStarted) {
                 currentLine++;
-                if (currentLine >= dialogueLines.Length) {
+                if (currentLine >= dialogueLines.Length && doChangeScene) {
+                    // change the scene
+                    dBox.SetActive(false);
+                    BlackBG.SetActive(false);
+                    currentLine = 0;
+                    GameManager.Instance.DialogActive = false;
+
+                    GameManager.Instance.PreviousScene = GameManager.Instance.CurrentScene;
+                    LoadSceneLogic.Instance.LoadScene(sceneName);
+                }
+                if (currentLine >= dialogueLines.Length && !needInput) {
                     dBox.SetActive(false);
                     BlackBG.SetActive(false);
 
                     currentLine = 0;
 
                     GameManager.Instance.DialogActive = false;
+                }
+                if (currentLine >= dialogueLines.Length && needInput && !ConfirmationPopup.activeSelf) {
+                    // if needs to show popup
+                    ConfirmationPopup.SetActive(true);
+                    eventSystem.SetSelectedGameObject(null);
+                    eventSystem.SetSelectedGameObject(YesButton);
+                    return;
                 }
                 showText();
             } else {
@@ -54,6 +87,7 @@ public class Dialogue : MonoBehaviour {
         dBox.SetActive(true);
         dialogueLines = lines;
         currentLine = 0;
+        needInput = false;
         showText();
         GameManager.Instance.DialogActive = true;
         // if triggered bc cutscene, want to set to false
@@ -70,6 +104,65 @@ public class Dialogue : MonoBehaviour {
         ClueImage.sprite = clueImage;
     }
 
+    /// <summary>
+    /// Return whether or not the dialogue is showing
+    /// </summary>
+    /// <returns>True if the dialogue is show, false otherwise</returns>
+    public bool GetDialogueActive() {
+        return dBox.activeSelf;
+    }
+
+    /// <summary>
+    /// Show the dialogue for going to the logic puzzle
+    /// </summary>
+    /// <param name="iniial">Dialogue for when first talking</param>
+    /// <param name="yes">Dialogue for choosing yes</param>
+    /// <param name="no">Dialogue for choosing no</param>
+    /// <param name="cannot">Dialogue for when player cannot go do the logic puzzle</param>
+    /// <param name="canDoPuzzle">True if player can go do logic puzzle, false otherwise</param>
+    /// <param name="sceneName">Name of the scene to go to for the logic puzzle</param>
+    public void ActivatePuzzleDialogue(string[] iniial, string[] yes, string[] no, string[] cannot, bool canDoPuzzle, string sceneName) {
+        this.yesDialogue = yes;
+        this.noDialogue = no;
+        this.cannotDialogue = cannot;
+        this.canDoPuzzle = canDoPuzzle;
+        this.sceneName = sceneName;
+        needInput = true;
+        doChangeScene = false;
+
+        dBox.SetActive(true);
+        dialogueLines = iniial;
+        currentLine = 0;
+        showText();
+        GameManager.Instance.DialogActive = true;
+        justStarted = true;
+    }
+
+    /// <summary>
+    /// Function for click on the confirmation popup buttons
+    /// </summary>
+    /// <param name="yes"></param>
+    public void ClickConfirmButtons(bool yes) {
+        if (yes) {
+            dialogueLines = canDoPuzzle ? this.yesDialogue : this.cannotDialogue;
+            currentLine = 0;
+            showText();
+            needInput = false;
+            doChangeScene = canDoPuzzle;
+        } else {
+            // click no
+            dialogueLines = this.noDialogue;
+            currentLine = 0;
+            showText();
+            needInput = false;
+        }
+        justStarted = true;
+        ConfirmationPopup.SetActive(false);
+    }
+
+    /// <summary>
+    /// Show the next line of text
+    /// </summary>
     private void showText() {
         string line = dialogueLines[currentLine];
         if (line.Contains("-")) {
@@ -82,9 +175,5 @@ public class Dialogue : MonoBehaviour {
             dName.text = "";
         }
         dText.text = line;
-    }
-
-    public bool GetDialogueActive() {
-        return dBox.activeSelf;
     }
 }
